27e2b2441cc92cfee3632474a71d11afd45794e0
Ashwini Bandgar <ashwinibandgar95@gmail.com>
2026-07-21 19:45:50 +0530

    Array Problems

---
+++ array/spiral_matrix.py
@@ -0,0 +1,29 @@
+def spiral_matrix(matrix):
+    ans=[]
+    top=0
+    bottom=len(matrix)-1
+    left=0
+    right=len(matrix[0])-1
+
+    while left<=right and top<=bottom:
+        for i in range(left,right+1):
+            ans.append(matrix[top][i])
+        top+=1
+        for j in range(top,bottom+1):
+            ans.append(matrix[j][right])
+        right-=1
+
+        if top<=bottom:
+            for i in range(right,left-1,-1):
+                ans.append(matrix[bottom][i])
+            bottom-=1
+
+        if left<=right:
+            for j in range(bottom,top-1,-1):
+                ans.append(matrix[j][left])
+            left+=1
+
+    return ans
+
+
+print(spiral_matrix([[1,2,3],[4,5,6],[7,8,9]]))
