c0b326b727c78b3082dbe42026a3e243a6188569
Ashwini Bandgar <ashwinibandgar95@gmail.com>
2026-08-01 20:53:54 +0530

    Java Practice

---
+++ Java/ifElse.java
@@ -0,0 +1,11 @@
+public class ifElse {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } else {
+            System.out.println("x is not greater than y");
+        }
+    }
+}
