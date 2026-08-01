81172098748da3d659e5ee37c2571786bb2c7187
Ashwini Bandgar <ashwinibandgar95@gmail.com>
2026-08-01 21:08:56 +0530

    Java Practice

---
+++ Java/LogicalOperator.java
@@ -0,0 +1,24 @@
+public class LogicalOperator {
+    public static void main(String[]args) {
+        boolean a=true;
+        boolean b=false;
+        if (a&&b){
+            System.out.println("True");
+        
+        }else{
+            System.out.println("False");
+        }
+
+        if(a||b){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+        if(!a){
+            System.out.println("True");}
+        else{
+            System.out.println("False");   } 
+
+
+    }
+}
