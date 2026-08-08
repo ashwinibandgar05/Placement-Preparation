030428aaf304b4f4c387059ba881da9d6b7c9503
Ashwini Bandgar <ashwinibandgar95@gmail.com>
2026-08-08 22:44:39 +0530

    String Problems

--- string/group_anagram.py
+++ string/group_anagram.py
@@ -1,3 +1,39 @@
+"""49. Group Anagrams
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ """
+
+
 def group_anagram(s):
     hashmap={}
 
@@ -17,3 +53,18 @@ def group_anagram(s):
 print(group_anagram(["eat","tea","tan","ate","nat","bat"]))
 
 
+# Other answers is 
+"""
+class Solution(object):
+    def groupAnagrams(self, strs):
+        
+        hashmap={}
+        for s in strs:
+            key="".join(sorted(s))
+            if key not in hashmap:
+                hashmap[key]=[]
+            hashmap[key].append(s)
+
+        return list(hashmap.values())
+        
+"""
